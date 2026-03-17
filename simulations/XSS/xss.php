6ebf83c4c8abfcb15418f77027b212c6675b249e
donsepe <sheldon.sepe@gmail.com>
2026-03-17 14:50:31 +0800

    Add xss.php for demonstrating XSS vulnerability
    Vulnerable web form script that accepts inputs without sanitization.

---
+++ simulations/XSS/xss.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<h2>Leave a comment</h2>
+
+<form method="GET">
+Name: <input type="text" name="name">
+<input type="submit">
+</form>
+
+Hello <?php echo $_GET['name']; ?>
+
+</body>
+</html>
